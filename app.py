b116a535b9fbc6457a68dba8699445ce2b71c8b3
archiewood <58074498+archiewood@users.noreply.github.com>
2020-04-12 11:55:51 +0100

    fixed date picker

--- app.py
+++ app.py
@@ -17,16 +17,16 @@ dates=df['date'].unique()
 
 app.layout = html.Div([
     html.Div([dcc.Dropdown(id='date-select', options=[{'label': i, 'value': i} for i in dates],
-                           value='TOR', style={'width': '140px'})]),
+                           value='2020-04-11', style={'width': '140px'})]),
     dcc.Graph('light-graph', config={'displayModeBar': False})])
 
 @app.callback(
     Output('light-graph', 'figure'),
     [Input('date-select','value')]
 )
-def update_graph(grpname):
+def update_graph(selected_date):
     import plotly.express as px
-    return px.line(df,x='time',y='light_reading',color='date')
+    return px.line(df[df.date==selected_date],x='time',y='light_reading',color='date')
 
 if __name__ == '__main__':
     app.run_server(debug=False)
