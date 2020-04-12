df2ead58a7411bce8e4f53c046b4c81aaf3588f6
archie <archiewood@outlook.com>
2020-04-12 13:37:26 +0100

    changing plots

--- app.py
+++ app.py
@@ -3,12 +3,25 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import plotly.express as px
+
 
 csv = 'https://raw.githubusercontent.com/archiewood/lightlogger/master/lightlog.csv'
 df = pd.read_csv(csv)
 df['date']=df['timestamp'].str[:10]
 df['time']=df['timestamp'].str[10:19]
-df.head()
+
+
+avg_window=5
+df['median']= df['light_reading'].rolling(avg_window).median()
+df['std'] = df['light_reading'].rolling(avg_window).std()
+
+df = df[(df.light_reading <= df['median']+3*df['std']) & (df.light_reading >= df['median']-3*df['std'])]
+
+df['light_reading_moving_avg'] = df.iloc[:,1].rolling(window=avg_window).mean()
+
+fig =px.line(df,x='timestamp',y='light_reading_moving_avg',title='Balcony Light Intensity',)
+
 
 app = dash.Dash(__name__)
 server = app.server
@@ -16,17 +29,8 @@ server = app.server
 dates=df['date'].unique()
 
 app.layout = html.Div([
-    html.Div([dcc.Dropdown(id='date-select', options=[{'label': i, 'value': i} for i in dates],
-                           value='2020-04-11', style={'width': '140px'})]),
-    dcc.Graph('light-graph', config={'displayModeBar': False})])
-
-@app.callback(
-    Output('light-graph', 'figure'),
-    [Input('date-select','value')]
+    dcc.Graph(figure=fig)]
 )
-def update_graph(selected_date):
-    import plotly.express as px
-    return px.line(df[df.date==selected_date],x='time',y='light_reading',color='date')
 
 if __name__ == '__main__':
     app.run_server(debug=False)
